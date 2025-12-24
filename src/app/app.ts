e48e41ed62ff99e6777ceec070d3d2d083dbdce4
Anna Vasilevich <dzichonka@gmail.com>
2025-12-24 18:08:16 +0100

    feat: add data input to results component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { ResultsComponent } from './results/results.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { type DataInput } from './models/data-input.model';
+import { type AnnualData } from './models/annual-data.model';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +18,11 @@ import { type DataInput } from './models/data-input.model';
   styleUrl: './app.scss',
 })
 export class App {
+  annualData: AnnualData[] = [];
   calculateInvestmentResults(data: DataInput): void {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
-    const annualData = [];
+    const annualData: AnnualData[] = [];
     let investmentValue = initialInvestment;
     for (let i = 0; i < duration; i++) {
       const year = i + 1;
@@ -37,6 +39,6 @@ export class App {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    console.log(annualData);
+    this.annualData = annualData;
   }
 }
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { type AnnualData } from '../models/annual-data.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss',
 })
 export class ResultsComponent {
-
+  @Input() results: AnnualData[] = [];
 }
