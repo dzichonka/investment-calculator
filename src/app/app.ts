90acb08cb75f555a657b6a3a376bb59ce3ee907a
Anna Vasilevich <dzichonka@gmail.com>
2025-12-24 16:37:58 +0100

    feat: add basic components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ResultsComponent } from "./results/results.component";
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ResultsComponent, UserInputComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
