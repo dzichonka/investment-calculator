e996e5553db89bfbd53b7a689eab592c59aa303e
Anna Vasilevich <dzichonka@gmail.com>
2025-12-24 17:57:17 +0100

    feat: add calculation logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { ResultsComponent } from "./results/results.component";
-import { UserInputComponent } from "./user-input/user-input.component";
+import { HeaderComponent } from './header/header.component';
+import { ResultsComponent } from './results/results.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { type DataInput } from './models/data-input.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ResultsComponent, UserInputComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ResultsComponent,
+    UserInputComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'investment-calculator';
+  calculateInvestmentResults(data: DataInput): void {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    console.log(annualData);
+  }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type DataInput } from '../models/data-input.model';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss',
 })
 export class UserInputComponent {
-
+  @Output() CalculatedData = new EventEmitter<DataInput>();
+  initialInvestment: string = '0';
+  annualInvestment: string = '0';
+  expectedReturn: string = '5';
+  duration: string = '10';
+  onSubmit(): void {
+    this.CalculatedData.emit({
+      initialInvestment: parseFloat(this.initialInvestment),
+      annualInvestment: parseFloat(this.annualInvestment),
+      expectedReturn: parseFloat(this.expectedReturn),
+      duration: parseInt(this.duration),
+    });
+  }
 }
